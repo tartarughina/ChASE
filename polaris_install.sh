f22834919301f5a5f607ca60fed6ff9be2a5b622
Riccardo Strina <riccardo.strina@icloud.com>
2024-08-26 16:07:05 +0200

    Update the build commands

--- polaris_install.sh
+++ polaris_install.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
+# Normal ChASE build
+
 cmake .. \
--DCMAKE_INSTALL_PREFIX=/home/tartarughina/ChASE-UM \
+-DCMAKE_INSTALL_PREFIX=/home/tartarughina/ChASE-def \
+-DBUILD_WITH_EXAMPLES=ON \
+-DNCCL_LIB_DIR=${NVIDIA_PATH}/comm_libs/nccl/lib \
+-DNCCL_INCLUDE_DIR=${NVIDIA_PATH}/comm_libs/nccl/include \
+-DCUDA_CUBLAS_LIBRARIES=${NVIDIA_PATH}/math_libs/lib64/libcublas.so \
+-DCUDA_cusolver_LIBRARY=${NVIDIA_PATH}/math_libs/lib64/libcusolver.so \
+-DCUDA_curand_LIBRARY=${NVIDIA_PATH}/math_libs/lib64/libcurand.so \
+-DCHASE_OUTPUT=ON
+
+# Unified Memory ChASE build
+
+cmake .. \
+-DCMAKE_INSTALL_PREFIX=/home/tartarughina/ChASE-um \
 -DBUILD_WITH_EXAMPLES=ON \
 -DNCCL_LIB_DIR=${NVIDIA_PATH}/comm_libs/nccl/lib \
 -DNCCL_INCLUDE_DIR=${NVIDIA_PATH}/comm_libs/nccl/include \
@@ -10,7 +24,22 @@ cmake .. \
 -DCUDA_curand_LIBRARY=${NVIDIA_PATH}/math_libs/lib64/libcurand.so \
 -DENABLE_UM=ON
 
+# Unified Memory Tuning ChASE build
+
+cmake .. \
+-DCMAKE_INSTALL_PREFIX=/home/tartarughina/ChASE-umt \
+-DBUILD_WITH_EXAMPLES=ON \
+-DNCCL_LIB_DIR=${NVIDIA_PATH}/comm_libs/nccl/lib \
+-DNCCL_INCLUDE_DIR=${NVIDIA_PATH}/comm_libs/nccl/include \
+-DCUDA_CUBLAS_LIBRARIES=${NVIDIA_PATH}/math_libs/lib64/libcublas.so \
+-DCUDA_cusolver_LIBRARY=${NVIDIA_PATH}/math_libs/lib64/libcusolver.so \
+-DCUDA_curand_LIBRARY=${NVIDIA_PATH}/math_libs/lib64/libcurand.so \
+-DENABLE_UM=ON \
+-DENABLE_TUNING=ON \
+-DCHASE_OUTPUT=ON
 # To ensure a correct build comment out or remove the lines setting cublas, cusolver and curand libraries
 # To enable UM and tuning add the following lines to the cmake command
 # -DENABLE_UM=ON
 # -DENABLE_TUNING=ON
+# For debugging purposes add the following line to the cmake command
+# -DCHASE_OUTPUT=ON
