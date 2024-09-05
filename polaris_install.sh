e1cf05b5a9f7d3619590e0f2b66258def7be5d79
Riccardo Strina <riccardo.strina@icloud.com>
2024-09-05 19:21:33 +0200

    Add custom include dir options in the CMake file

--- polaris_install.sh
+++ polaris_install.sh
@@ -10,6 +10,7 @@ cmake .. \
 -DBUILD_WITH_EXAMPLES=ON \
 -DNCCL_LIB_DIR=${NVIDIA_PATH}/comm_libs/nccl/lib \
 -DNCCL_INCLUDE_DIR=${NVIDIA_PATH}/comm_libs/nccl/include \
+-DMATH_LIBS_INCLUDE_DIR=${NVIDIA_PATH}/math_libs/include \
 -DCUDA_CUBLAS_LIBRARIES=${NVIDIA_PATH}/math_libs/lib64/libcublas.so \
 -DCUDA_cusolver_LIBRARY=${NVIDIA_PATH}/math_libs/lib64/libcusolver.so \
 -DCUDA_curand_LIBRARY=${NVIDIA_PATH}/math_libs/lib64/libcurand.so \
