7a7e9054b5713c0a3b795638e36cce71f93ce594
Riccardo Strina <riccardo.strina@icloud.com>
2024-09-26 20:51:41 +0200

    Add debug messages

--- ChASE-MPI/impl/chase_mpidla_mgpu_um.hpp
+++ ChASE-MPI/impl/chase_mpidla_mgpu_um.hpp
@@ -378,11 +378,11 @@ public:
         cuda_exec(cudaMallocManaged((void**)&d_off_n_,
                                     diag_off_size_ * sizeof(std::size_t)));
 
-        cout << "diag_off_size_ = " << diag_off_size_ << endl;
-        cout << d_off_m_ << " " << d_off_n_ << endl;
-        cout << d_off_n_ << " " << d_off_m_ << endl;
-        cout << off_m << endl;
-        cout << off_n << endl;
+        std::cout << "diag_off_size_ = " << diag_off_size_ << std::endl;
+        std::cout << d_off_m_ << " " << d_off_n_ << std::endl;
+        std::cout << d_off_n_ << " " << d_off_m_ << std::endl;
+        std::cout << off_m << std::endl;
+        std::cout << off_n << std::endl;
 
         std::copy(off_m.begin(), off_m.end(), d_off_m_);
         std::copy(off_n.begin(), off_n.end(), d_off_n_);
